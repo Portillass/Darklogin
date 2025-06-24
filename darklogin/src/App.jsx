37d6af699bf7b4e4469afa634d4c55e3d80dec8c
JARELL E. PORTILLAS <115545323+Portillass@users.noreply.github.com>
2025-06-24 21:42:12 +0800

    Update App.jsx

--- darklogin/src/App.jsx
+++ darklogin/src/App.jsx
@@ -1,14 +1,16 @@
 //importing the CSS styles
 import './App.css'
 
-
+// Main nga function component sa React
 function App() {
   return (
     <>
-      <h1>darklogin</h1>
-      <h2>for nothing</h2>
+       {/* Title sa imong app, gi-display sa taas */}
+        <h1>darklogin</h1>
+          {/* Subtitle or subheading para sa app */}
+        <h2>for nothing</h2>
     </>
   )
 }
-
+// Gi-export ang App aron magamit sa ubang parte sa project
 export default App
